a5d5a6697a8ca07c7e9aee54e241306e527ff970
jiashinchan <117286786+jiashinchan@users.noreply.github.com>
2023-06-12 16:25:28 +0800

    Update EngagingCaoCao.java

--- EngagingCaoCao.java
+++ EngagingCaoCao.java
@@ -23,10 +23,10 @@ public class EngagingCaoCao {
         {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
     };
 
-    private static final int ROWS = Maze.length;
-    private static final int COLS = Maze[0].length;
-    private static final int START = 2;
-    private static final int EXIT = 3;  
+    private static final int rows = Maze.length;
+    private static final int cols = Maze[0].length;
+    private static final int start = 2;
+    private static final int exit = 3;  
 
     private static List<Integer> path;
     static Scanner engage = new Scanner(System.in);
@@ -39,8 +39,8 @@ public class EngagingCaoCao {
                            Show how Cao Cao might have retreated from Hua Rong Road so that Liu Bei and 
                            Zhao Yun can catch up with him. Besides, Guan Yu is ahead and is engaging Cao Cao 
                            at the exit of the maze.""");
-        System.out.println("\n 2D maze of Hua Rong Road: ");
-        System.out.println("\n (2 denotes the starting point. 3 denotes the exit of the maze)");
+        System.out.println("\n2D maze of Hua Rong Road: ");
+        System.out.println("*2 denotes the starting point,3 denotes the exit of the maze*");
         
         for (int row = 0; row < Maze.length; row++) {
             for (int col = 0; col < Maze[row].length; col++) {
@@ -48,7 +48,7 @@ public class EngagingCaoCao {
             }
             System.out.println();
         }
-        System.out.println("\n--------------------------------------------------------\n");
+        System.out.println("\n----------------------------------------------------------------------------------------\n");
 
         System.out.println("Enter to find the path: ");
         engage.nextLine();
@@ -61,14 +61,14 @@ public class EngagingCaoCao {
     private static List <Integer> findPath(){
         Queue <Integer> positionQueue = new LinkedList<>();
         Map <Integer, Integer> MapPositions = new HashMap();
-        boolean [][] alreadyVisited = new boolean [ROWS][COLS];
+        boolean [][] alreadyVisited = new boolean [rows][cols];
         
         int startRow =-1, startCol=-1;
         
         // Locating the starting position
-    for (int row = 0; row < ROWS; row++) {
-        for (int col = 0; col < COLS; col++) {
-            if (Maze[row][col] == START) {
+        for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            if (Maze[row][col] == start) {
                 startRow = row;
                 startCol = col;
                 break;
@@ -76,28 +76,28 @@ public class EngagingCaoCao {
         }
     }
         // Return empty list if no valid starting point is found
-    if (startRow == -1 || startCol == -1) {
-        return new ArrayList<>(); 
+        if (startRow == -1 || startCol == -1) {
+            return new ArrayList<>(); 
     }
         
         //Breadth-First Search(BFS)
-        positionQueue.offer(startRow * COLS + startCol);
+        positionQueue.offer(startRow * cols + startCol);
         alreadyVisited[startRow][startCol] = true;
 
-    while (!positionQueue.isEmpty()) {
-        int currentPosition = positionQueue.poll();
-        int row = currentPosition / COLS;
-        int col = currentPosition % COLS;
+        while (!positionQueue.isEmpty()) {
+            int currentPosition = positionQueue.poll();
+            int row = currentPosition / cols;
+            int col = currentPosition % cols;
         
         // If the current position is the exit
-        if (Maze[row][col] == EXIT) {
+        if (Maze[row][col] == exit) {
             List<Integer> path = new ArrayList<>();
             int current = currentPosition;
-            while (current != startRow * COLS + startCol) {
+            while (current != startRow * cols + startCol) {
                 path.add(current);
                 current = MapPositions.get(current);
             }
-            path.add(startRow * COLS + startCol);
+            path.add(startRow * cols + startCol);
             Collections.reverse(path);
             return path; // Return path
         }
@@ -108,9 +108,9 @@ public class EngagingCaoCao {
         for (int i = 0; i < 4; i++) {
             int newRow = row + rowShifts[i];
             int newCol = col + colShifts[i];
-            int newPosition = newRow * COLS + newCol;
+            int newPosition = newRow * cols + newCol;
 
-            if (newRow >= 0 && newRow < ROWS && newCol >= 0 && newCol < COLS && Maze[newRow][newCol] != 1
+            if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && Maze[newRow][newCol] != 1
                     && !alreadyVisited[newRow][newCol]) {
                 positionQueue.offer(newPosition);
                 alreadyVisited[newRow][newCol] = true;
@@ -118,15 +118,16 @@ public class EngagingCaoCao {
             }
         }
     }
-    return new ArrayList<>(); // Return empty list if no path is found
-}
+        return new ArrayList<>(); // Return empty list if no path is found
+    }
+    
     private static void displayPath() {
     // Initialize a 2D char array for path visualization
-    char[][] visualizedPath = new char[ROWS][COLS];
+    char[][] visualizedPath = new char[rows][cols];
 
     // Fill the 2D array with the maze layout
-    for (int row = 0; row < ROWS; row++) {
-        for (int col = 0; col < COLS; col++) {
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
             if (Maze[row][col] == 1 || Maze[row][col] == 0) {
                 visualizedPath[row][col] = (char) (Maze[row][col] + '0'); // Casting 1 and 0 to their char equivalents
             } else {
@@ -137,21 +138,22 @@ public class EngagingCaoCao {
 
     // Mark the escape path on the visualized path
     for (int position : path) {
-        int row = position / COLS;
-        int col = position % COLS;
+        int row = position / cols;
+        int col = position % cols;
         visualizedPath[row][col] = '#'; // Using dot as path marker
     }
 
     // Printing the path matrix
     System.out.println("\nPath of how Cao Cao might have escaped:  ");
     System.out.println("Note: '#' represents the path");
-    for (int row = 0; row < ROWS; row++) {
-        for (int col = 0; col < COLS; col++) {
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
             System.out.print(visualizedPath[row][col] + " ");
         }
         System.out.println();
     }
-    System.out.println("\n------------------------------------------------------------\n");
+    System.out.print("\nPress Enter to go back to Main");
+    engage.nextLine();
+    System.out.println("\n-----------------------------------------------------------------------------------\n");
     }
 }
-
